679a086b3080646b37c988767f810b207348db37
cristianolongo <cristianolongo@gmail.com>
2017-05-04 14:46:34 +0200

    totem albo pop

---
+++ totem/somecatania.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * A page to share feed items of facebook.
+ * 
+ * Copyright 2016 Cristiano Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//TODO remove
+//define ("RSSPATH","http://dev.opendatasicilia.it/albopop/catania/");
+
+$feeds=array(
+	"http://dev.opendatasicilia.it/albopop/unict/unict2RSS.php",
+	"http://dev.opendatasicilia.it/albopop/catania/alboct2RSS.php",
+	"http://dev.opendatasicilia.it/albopop/cittametropolitanacatania/albofeed.php");
+
+//choose a feed random
+$selectedFeedNumber = rand ( 0, count($feeds)-0.1 );
+	
+$feedUrl=$feeds[$selectedFeedNumber];
+
+require("totem-template.php");
+?>
---
+++ totem/totem-template.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * A template to show a random notice from a notice board.
+ * Just set the following variables and import this file in your own sharer.php page:
+ * 
+ * - feedUrl url of the feed
+ * - title of the albo
+ * - logo an url of the page logo
+ * - credits html code to be put in the Credits section
+ * 
+ * We are using https://github.com/dg/rss-php/ to parse rss feeds
+ *
+ * Copyright 2017 Cristiano Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once '../rss-php/src/Feed.php';
+
+/**
+ * Get a random entry from a feed.
+ *
+ * @param string $feedUrl        	
+ */
+function getRandomFeedEntry($feedUrl) {
+	$feed = Feed::load ( $feedUrl );
+	
+	if (count ( $feed->entry ) > 0)
+		$entries = $feed->entry;
+	else if (count ( $feed->item ) > 0)
+		$entries = $feed->item;
+	else {
+		echo "No entries";
+		return false;
+	}
+	
+	$totalEntries = count ( $entries );
+	$selectedEntryNumber = rand ( 0, $totalEntries );
+	return $entries [$selectedEntryNumber];
+}
+
+if (isset ( $ente )) {
+	$donationTxt = "SOSTIENI ALBO POP $ente CON UNA DONAZIONE AD HACKSPACE CATANIA";
+	$motivation = "Supporto per opendatahacklab e Albo POP $ente";
+} else {
+	$donationTxt = "SOSTIENI QUESTO ALBO POP CON UNA DONAZIONE AD HACKSPACE CATANIA";
+	$motivation = "Supporto per opnedatahacklab";
+}
+$feed = Feed::load ( $feedUrl );
+$entry = getRandomFeedEntry ( $feedUrl );
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo $feed->title;?></title>
+<link rel="stylesheet" type="text/css" href="totem.css" />
+<meta http-equiv="refresh" content="10">
+</head>
+<body>
+	<header class="main-header" id="top">
+		<img class="logo" src="http://albopop.it/images/logo.png" alt="logo albo pop" />
+		<h1>
+			<?php echo $feed->title;?>
+		</h1>
+		<form class="donation" action="https://www.paypal.com/cgi-bin/webscr"
+			method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick" /> <input
+				type="hidden" name="hosted_button_id" value="CXAF33S5R9QZE" /> <input
+				type="hidden" name="item_name" value="<?php echo $motivation; ?>" />
+			<input class="donation" type="submit" name="sumbit"
+				value="<?php echo $donationTxt; ?>" /> </label>
+		</form>
+	</header>
+	<?php
+	
+	if (isset ( $news ))
+		echo "\t<section id=\"news\">\n\t\t<p>$news</p>\n\t</section>\n";
+	?>
+	<section id="avviso">
+		<blockquote cite="<?php echo $entry->link?>">
+			<p>
+				<?php echo $entry->description; ?>
+			</p>
+		</blockquote>
+	</section>
+
+	<section id="credits">
+		<p>
+			Per saperne di pi&ugrave visita il sito
+			<code>albopop.it</code>
+			o vieni a trovarci in via Grotte Bianche 112 a Catania.
+			Questo albo pop &egrave; stato realizzato da <a
+				href="http://hackspacecatania.it" target="_blank">Hackspace Catania</a>
+			nell'ambito del progetto <a href="http://opendatahacklab.org"
+				target="_blank"><code>opendatahacklab</code> </a>
+		</p>
+		<p class="links">
+			<a href="http://hackspacecatania.it/" target="_blank"> <img
+				src="http://hackspacecatania.it/wp-content/uploads/2014/04/logo-hackspace-learn1.png"
+				alt="LEARN MAKE HACK SHARE. Hackspace Catania" />
+			</a> <a href="http://opendatahacklab.org" target="_blank"> <img
+				src="http://opendatahacklab.org/commons/imgs/logo_cog4_ter.png"
+				alt="logo opendatahacklab" />
+			</a>
+		</p>
+	</section>
+
+</body>
+</html>
+
