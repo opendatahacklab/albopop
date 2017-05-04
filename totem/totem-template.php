8ef59658d7fc51ee61cd609d4a87d757e268cdaf
cristianolongo <cristianolongo@gmail.com>
2017-05-04 15:04:16 +0200

    more on totem

--- totem/totem-template.php
+++ totem/totem-template.php
@@ -25,7 +25,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-require_once '../rss-php/src/Feed.php';
+require_once '../rss-php/Feed.php';
 
 /**
  * Get a random entry from a feed.
