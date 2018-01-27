a55b15e198e71fe34e3d9dfa3a4055afc5df8773
Cristiano Longo <cristianolongo@hackspacecatania.it>
2018-01-27 16:53:55 +0100

    RDF/XML translation of the dataset of schools in the municipality of Catania

---
+++ schools/School.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  A school in the sense of http://dati.istruzione.it/opendata/opendata/catalogo/elements1/leaf/?datasetId=DS0400SCUANAGRAFESTAT
+ *
+ * Copyright 2018 Cristiano Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Cristiano Longo 
+ */
+
+class School{
+	public $codiceistitutoriferimento;
+	public $codicescuola;
+	public $denominazione;
+
+	function __construct($row){
+		$this->codiceistitutoriferimento=$row[4];
+		$this->codicescuola=$row[6];
+		$this->denominazionescuola=$row[7];
+	}
+}
+?>
